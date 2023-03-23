cc59108e934711d64e2985a5e09d2e2f13f1441e
21ulwe <21ulwe@stockholmscience.se>
2023-03-23 11:42:02 +0100

    slakt

--- src/components/dashboard/widget/Widget.tsx
+++ src/components/dashboard/widget/Widget.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import "./Widget.css"
+import React, { useState } from "react";
+import "./Widget.css";
+import { Line } from "react-chartjs-2";
 
 export default function Widget() {
   return (
-    <div className='container'>Widgets</div>
-    
-  )
+    <div className="container">
+      <h2>Air Pressure:</h2>
+      <div className="main-content">
+        <div style={{width:"70px"}}>
+          <div>Pressure</div>
+          <div>PA</div>
+        </div>
+        <div>
+          för i helvete
+        </div>
+      </div>
+
+    </div>
+  );
 }
