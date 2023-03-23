99d19630662cac8a99a976fd4c5932438ccd610c
Wilgot Wardell <wilgotwardell@hotmail.com>
2023-03-23 10:03:26 +0100

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Menu from './components/menu/Menu';
 
 function App() {
   return (
     <div className="App">
-      
+      <Menu/>
     </div>
   );
 }
