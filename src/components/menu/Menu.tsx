ba90d0d0d124c5a4b1432eefacc4194a3a1af5a8
Wilgot Wardell <wilgotwardell@hotmail.com>
2023-03-23 10:45:17 +0100

    -

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import "./Menu.css"
+import myImage from './plantan.png';
+
 
 export default function Menu() {
   return (
       <div className='menu-container'>
+        <img src={myImage} alt="" />
         <h3>fiskarnas water system</h3>
       </div>
   )
