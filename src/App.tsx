aeda5c03bc487442bc2a4f3c52f6d03660c2b866
21ulwe <21ulwe@stockholmscience.se>
2023-03-23 10:09:01 +0100

    dashboard added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      
+      <Dashboard/>
     </div>
   );
 }
