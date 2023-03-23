3cf9a4d544eee8059bca29207dd90fb2fdeac304
21ulwe <21ulwe@stockholmscience.se>
2023-03-23 13:42:16 +0100

    slak

--- src/components/dashboard/widget/WidgetContainer.tsx
+++ src/components/dashboard/widget/WidgetContainer.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Widget from "./Widget";
-import "./WidgetContainer.css"
+import "./WidgetContainer.css";
 export default function WidgetContainer() {
   return (
     <div className="widget-container">
-      <Widget Title="Air pressure" Info="Pressure" Unit="PA" />
-      <Widget Title="Humidity" Info="Humidity" Unit="(%)"/>
-      <Widget Title="Temperature" Info="Temp" Unit="C*"/>
-      <Widget Title="Information" Info=" " Unit=" "/>
+      <h1 style={{marginBottom:"20px"}}>Dashboard: System 1</h1>
+      <div className="widget-grid">
+        <Widget Title="Air pressure" Info="Pressure" Unit="PA" />
+        <Widget Title="Humidity" Info="Humidity" Unit="(%)" />
+        <Widget Title="Temperature" Info="Temp" Unit="C*" />
+        <Widget Title="Information" Info=" " Unit=" " />
+      </div>
     </div>
   );
 }
