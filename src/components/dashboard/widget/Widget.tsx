eb171483a1729ec4e4653d9fb7d4e8be58f5f3e5
Wilgot Wardell <wilgotwardell@hotmail.com>
2023-03-23 14:13:05 +0100

    .

--- src/components/dashboard/widget/Widget.tsx
+++ src/components/dashboard/widget/Widget.tsx
@@ -1,43 +1,40 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./Widget.css";
-import { Line } from "react-chartjs-2";
-
-interface ChartData {
-  labels: (string | Date)[];
-  datasets: {
-    label: string;
-    data: (string | number)[];
-    fill: boolean;
-    borderColor: string;
-    tension: number;
-  }[];
-}
-
-export default function Widget({} : {Title: string, Info: string, Unit: string}) {
-  const [chartData, setChartData] = useState<ChartData>({
-    labels: ["1", "2", "3", "4", "5"],
-    datasets: [
-      {
-        label: "Air Pressure",
-        data: [9800, 98081, 98081, 98090, 98081],
-        fill: false,
-        borderColor: "rgba(75,192,192,1)",
-        tension: 0.1,
-      },
-    ],
-  });
-
-  
-
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { LineGraph } from "./Line";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+export default function Widget({}: {
+  Title: string;
+  Info: string;
+  Unit: string;
+}) {
   return (
     <div className="container">
       <h2>Air Pressure:</h2>
       <div className="main-content">
-        <div style={{ width: "70px" }}>
+        <div>
           <div>Pressure</div>
           <div>PA</div>
+          <LineGraph />
         </div>
-        <Line data={chartData} />
       </div>
-    </div>)
- }
+    </div>
+  );
+}
